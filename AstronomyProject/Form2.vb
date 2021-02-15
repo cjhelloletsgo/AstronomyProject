3a88905beebda3042a6f7fe80615f419f504885c
Colton McLaughlin <cjr2d29719@gmail.com>
2021-02-15 14:24:34 -0700

    First commit in colton branch, added results to some answer buttons

--- AstronomyProject/Form2.vb
+++ AstronomyProject/Form2.vb
@@ -1,19 +1,21 @@
 ﻿Public Class Form2
     Dim blink As Boolean
     Dim count As Integer
+    Dim Word1 As String
+    Dim Word2 As String
+    Dim Word3 As String
     Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Wrong.Visible = False
+        FireworksGif.Visible = False
     End Sub
 
     Private Sub ToStartingForm_Click(sender As Object, e As EventArgs) Handles ToStartingForm.Click
         StartingForm.Show()
         Me.Hide()
     End Sub
-    Dim Word1 As String
-    Dim Word2 As String
-    Dim Word3 As String
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
         Word1 = "Kid"
         Word2 = "You"
         Word3 = "Suck"
@@ -30,7 +32,6 @@
                     Wrong.Text = Word3
                 Case Else
                     Wrong.Visible = False
-
             End Select
 
             If (blink) Then
@@ -54,10 +55,14 @@
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Timer1.Enabled = True
-        Timer1.Interval = 500
+        Timer1.Interval = 750
         Me.BackColor = Color.Black
         blink = True
         count = 0
         Wrong.Visible = True
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        FireworksGif.Visible = True
     End Sub
 End Class
--- AstronomyProject/Form3.Designer.vb
+++ AstronomyProject/Form3.Designer.vb
@@ -22,11 +22,18 @@ Partial Class Form3
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form3))
         Me.ToStartingForm = New System.Windows.Forms.Button()
         Me.RevolutionLabel = New System.Windows.Forms.Label()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.RevolutionButton = New System.Windows.Forms.Button()
+        Me.RotationButton = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Wrong = New System.Windows.Forms.Label()
+        Me.FireworksGif = New System.Windows.Forms.PictureBox()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.FireworksGif, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'ToStartingForm
@@ -51,24 +58,70 @@ Partial Class Form3
         'PictureBox1
         '
         Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
-        Me.PictureBox1.Location = New System.Drawing.Point(154, 194)
+        Me.PictureBox1.Location = New System.Drawing.Point(228, 230)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(556, 339)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         Me.PictureBox1.TabIndex = 3
         Me.PictureBox1.TabStop = False
         '
+        'RevolutionButton
+        '
+        Me.RevolutionButton.Location = New System.Drawing.Point(282, 141)
+        Me.RevolutionButton.Name = "RevolutionButton"
+        Me.RevolutionButton.Size = New System.Drawing.Size(91, 29)
+        Me.RevolutionButton.TabIndex = 4
+        Me.RevolutionButton.Text = "Revolution"
+        Me.RevolutionButton.UseVisualStyleBackColor = True
+        '
+        'RotationButton
+        '
+        Me.RotationButton.Location = New System.Drawing.Point(482, 141)
+        Me.RotationButton.Name = "RotationButton"
+        Me.RotationButton.Size = New System.Drawing.Size(102, 29)
+        Me.RotationButton.TabIndex = 5
+        Me.RotationButton.Text = "Rotation"
+        Me.RotationButton.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        '
+        'Wrong
+        '
+        Me.Wrong.AutoSize = True
+        Me.Wrong.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!)
+        Me.Wrong.Location = New System.Drawing.Point(624, 167)
+        Me.Wrong.Name = "Wrong"
+        Me.Wrong.Size = New System.Drawing.Size(160, 36)
+        Me.Wrong.TabIndex = 10
+        Me.Wrong.Text = "For Shame"
+        '
+        'FireworksGif
+        '
+        Me.FireworksGif.Image = CType(resources.GetObject("FireworksGif.Image"), System.Drawing.Image)
+        Me.FireworksGif.Location = New System.Drawing.Point(12, 12)
+        Me.FireworksGif.Name = "FireworksGif"
+        Me.FireworksGif.Size = New System.Drawing.Size(1084, 703)
+        Me.FireworksGif.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.FireworksGif.TabIndex = 11
+        Me.FireworksGif.TabStop = False
+        '
         'Form3
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1186, 783)
+        Me.Controls.Add(Me.Wrong)
+        Me.Controls.Add(Me.RotationButton)
+        Me.Controls.Add(Me.RevolutionButton)
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.RevolutionLabel)
         Me.Controls.Add(Me.ToStartingForm)
+        Me.Controls.Add(Me.FireworksGif)
         Me.Name = "Form3"
         Me.Text = "Form3"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.FireworksGif, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -77,4 +130,9 @@ Partial Class Form3
     Friend WithEvents ToStartingForm As Button
     Friend WithEvents RevolutionLabel As Label
     Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents RevolutionButton As Button
+    Friend WithEvents RotationButton As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Wrong As Label
+    Friend WithEvents FireworksGif As PictureBox
 End Class
