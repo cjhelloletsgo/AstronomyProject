7b3fef4649f87933c940a91d23a9fde6d279fb8c
Colton McLaughlin <cjr2d29719@gmail.com>
2021-03-11 17:07:32 -0700

    cleaned up forms

--- AstronomyProject/Lessons.Designer.vb
+++ AstronomyProject/Lessons.Designer.vb
@@ -23,29 +23,82 @@ Partial Class Lessons
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.HomeButton = New System.Windows.Forms.Button()
+        Me.LessonsLabel = New System.Windows.Forms.Label()
+        Me.Lesson1Button = New System.Windows.Forms.Button()
+        Me.Lesson2Button = New System.Windows.Forms.Button()
+        Me.Lesson3Button = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'HomeButton
         '
         Me.HomeButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
-        Me.HomeButton.Location = New System.Drawing.Point(655, 339)
+        Me.HomeButton.Location = New System.Drawing.Point(665, 406)
         Me.HomeButton.Name = "HomeButton"
-        Me.HomeButton.Size = New System.Drawing.Size(99, 53)
+        Me.HomeButton.Size = New System.Drawing.Size(123, 32)
         Me.HomeButton.TabIndex = 0
         Me.HomeButton.Text = "Home"
         Me.HomeButton.UseVisualStyleBackColor = True
+        '
+        'LessonsLabel
+        '
+        Me.LessonsLabel.AutoSize = True
+        Me.LessonsLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!)
+        Me.LessonsLabel.Location = New System.Drawing.Point(337, 9)
+        Me.LessonsLabel.Name = "LessonsLabel"
+        Me.LessonsLabel.Size = New System.Drawing.Size(127, 36)
+        Me.LessonsLabel.TabIndex = 1
+        Me.LessonsLabel.Text = "Lessons"
+        '
+        'Lesson1Button
+        '
+        Me.Lesson1Button.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Lesson1Button.Location = New System.Drawing.Point(142, 182)
+        Me.Lesson1Button.Name = "Lesson1Button"
+        Me.Lesson1Button.Size = New System.Drawing.Size(123, 32)
+        Me.Lesson1Button.TabIndex = 2
+        Me.Lesson1Button.Text = "Lesson 1"
+        Me.Lesson1Button.UseVisualStyleBackColor = True
+        '
+        'Lesson2Button
+        '
+        Me.Lesson2Button.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Lesson2Button.Location = New System.Drawing.Point(339, 182)
+        Me.Lesson2Button.Name = "Lesson2Button"
+        Me.Lesson2Button.Size = New System.Drawing.Size(123, 32)
+        Me.Lesson2Button.TabIndex = 3
+        Me.Lesson2Button.Text = "Lesson 2"
+        Me.Lesson2Button.UseVisualStyleBackColor = True
+        '
+        'Lesson3Button
+        '
+        Me.Lesson3Button.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Lesson3Button.Location = New System.Drawing.Point(536, 182)
+        Me.Lesson3Button.Name = "Lesson3Button"
+        Me.Lesson3Button.Size = New System.Drawing.Size(123, 32)
+        Me.Lesson3Button.TabIndex = 4
+        Me.Lesson3Button.Text = "Lesson 3"
+        Me.Lesson3Button.UseVisualStyleBackColor = True
         '
         'Lessons
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Lesson3Button)
+        Me.Controls.Add(Me.Lesson2Button)
+        Me.Controls.Add(Me.Lesson1Button)
+        Me.Controls.Add(Me.LessonsLabel)
         Me.Controls.Add(Me.HomeButton)
         Me.Name = "Lessons"
         Me.Text = "Lessons"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents HomeButton As Button
+    Friend WithEvents LessonsLabel As Label
+    Friend WithEvents Lesson1Button As Button
+    Friend WithEvents Lesson2Button As Button
+    Friend WithEvents Lesson3Button As Button
 End Class
--- AstronomyProject/Quizzes.Designer.vb
+++ AstronomyProject/Quizzes.Designer.vb
@@ -24,12 +24,13 @@ Partial Class Quizzes
     Private Sub InitializeComponent()
         Me.HomeButton = New System.Windows.Forms.Button()
         Me.ShootingStarsButton = New System.Windows.Forms.Button()
+        Me.QuizzesLabel = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'HomeButton
         '
         Me.HomeButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
-        Me.HomeButton.Location = New System.Drawing.Point(640, 343)
+        Me.HomeButton.Location = New System.Drawing.Point(682, 392)
         Me.HomeButton.Name = "HomeButton"
         Me.HomeButton.Size = New System.Drawing.Size(106, 46)
         Me.HomeButton.TabIndex = 0
@@ -38,26 +39,39 @@ Partial Class Quizzes
         '
         'ShootingStarsButton
         '
-        Me.ShootingStarsButton.Location = New System.Drawing.Point(288, 118)
+        Me.ShootingStarsButton.Location = New System.Drawing.Point(332, 234)
         Me.ShootingStarsButton.Name = "ShootingStarsButton"
         Me.ShootingStarsButton.Size = New System.Drawing.Size(121, 36)
         Me.ShootingStarsButton.TabIndex = 1
         Me.ShootingStarsButton.Text = "Shooting Stars"
         Me.ShootingStarsButton.UseVisualStyleBackColor = True
         '
+        'QuizzesLabel
+        '
+        Me.QuizzesLabel.AutoSize = True
+        Me.QuizzesLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!)
+        Me.QuizzesLabel.Location = New System.Drawing.Point(331, 22)
+        Me.QuizzesLabel.Name = "QuizzesLabel"
+        Me.QuizzesLabel.Size = New System.Drawing.Size(122, 36)
+        Me.QuizzesLabel.TabIndex = 2
+        Me.QuizzesLabel.Text = "Quizzes"
+        '
         'Quizzes
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.QuizzesLabel)
         Me.Controls.Add(Me.ShootingStarsButton)
         Me.Controls.Add(Me.HomeButton)
         Me.Name = "Quizzes"
         Me.Text = "Quizzes"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents HomeButton As Button
     Friend WithEvents ShootingStarsButton As Button
+    Friend WithEvents QuizzesLabel As Label
 End Class
