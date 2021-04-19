97886937372f2ec8ae439bdf83c8ac4fbf4e66d4
Colton McLaughlin <cjr2d29719@gmail.com>
2021-04-19 15:01:13 -0600

    question averages starting to work

--- AstronomyProject/Teacher.Designer.vb
+++ AstronomyProject/Teacher.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Teacher
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class Teacher
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.quiz1Label = New System.Windows.Forms.Label()
         Me.quiz2Label = New System.Windows.Forms.Label()
@@ -32,6 +32,9 @@ Partial Class Teacher
         Me.quiz1Result = New System.Windows.Forms.Label()
         Me.quiz2Result = New System.Windows.Forms.Label()
         Me.quiz3Result = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -64,7 +67,7 @@ Partial Class Teacher
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(174, 329)
+        Me.Button1.Location = New System.Drawing.Point(24, 257)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 3
@@ -124,11 +127,41 @@ Partial Class Teacher
         Me.quiz3Result.TabIndex = 9
         Me.quiz3Result.Text = "blank"
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(60, 227)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(159, 227)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(249, 219)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "Label3"
+        '
         'Teacher
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.quiz3Result)
         Me.Controls.Add(Me.quiz2Result)
         Me.Controls.Add(Me.quiz1Result)
@@ -157,4 +190,7 @@ Partial Class Teacher
     Friend WithEvents quiz1Result As Label
     Friend WithEvents quiz2Result As Label
     Friend WithEvents quiz3Result As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
 End Class
--- AstronomyProject/Teacher.vb
+++ AstronomyProject/Teacher.vb
@@ -18,20 +18,19 @@
         Dim myda As New OleDb.OleDbDataAdapter
         Dim sql As String
 
-        sql = "Select * From lesson1Questions"
-
         ''For eace student
         'sql = "Select Distinct Case S1.StudentID, S1.Grade As quiz1Grade, S2.Grade As quiz2Grade, S3.Grade As quiz3Grade
         'From StudentQuestions1 As S1, StudentQuestions2 As S2, StudentQuestions3 As S3"
 
         'Average score per question
-        sql = "Select Case Abs((Avg(Q1))), Abs(Avg(Q2)), Abs((Avg(Q3))), Abs((Avg(Q3))), Abs((Avg(Q4))), Abs((Avg(Q5))),  Abs((Avg(Q6))),  Abs((Avg(Q7))),  Abs((Avg(Q8))),  Abs((Avg(Q9))),  Abs((Avg(Q10))),  Abs((Avg(Q11))),  Abs((Avg(Q12))),  Abs((Avg(Q13))),  Abs((Avg(Q14))), Abs((Avg(Q15)))
+        sql = "Select Abs((Avg(Q1))), Abs(Avg(Q2)), Abs((Avg(Q3))), Abs((Avg(Q3))), Abs((Avg(Q4))), Abs((Avg(Q5))),  Abs((Avg(Q6))),  Abs((Avg(Q7))),  Abs((Avg(Q8))),  Abs((Avg(Q9))),  Abs((Avg(Q10))),  Abs((Avg(Q11))),  Abs((Avg(Q12))),  Abs((Avg(Q13))),  Abs((Avg(Q14))), Abs((Avg(Q15)))
         From StudentQuestions1"
-
-
-
         myda = New OleDb.OleDbDataAdapter(sql, mycon)
-        myda.Fill(myds, "MyQuestions")
+        myda.Fill(myds, "Teacher")
+
+        Label1.Text = myds.Tables("Teacher").Rows(0).Item(0)
+        Label2.Text = myds.Tables("Teacher").Rows(0).Item(1)
+        Label3.Text = myds.Tables("Teacher").Rows(0).Item(2)
 
         mycon.Close()
     End Sub
