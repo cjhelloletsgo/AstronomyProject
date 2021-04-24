a434914991b47b09530b03c0d345da34323d10b2
bee <breeannettejohnson@gmail.com>
2021-04-24 11:35:14 -0600

    hydra

--- AstronomyProject/Lessons/Lesson3/pg6Lesson3.Designer.vb
+++ AstronomyProject/Lessons/Lesson3/pg6Lesson3.Designer.vb
@@ -118,6 +118,7 @@ Partial Class pg6Lesson3
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(628, 542)
         Me.Label1.TabIndex = 46
+        Me.Label1.Text = resources.GetString("Label1.Text")
         '
         'pg6Lesson3
         '
